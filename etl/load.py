3d6514449af69c84b5f6bd268ae34202912eddfa
bezwymowki <klaudia.adamczyk707@gmail.com>
2026-05-07 23:40:30 +0200

    Made minor changes

--- etl/load.py
+++ etl/load.py
@@ -1,12 +1,11 @@
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from psycopg2.extras import execute_values
 from airflow.decorators import task
-from utils.files_io import read_csv
 
 @task
-def load_data(category):
-    data = read_csv(category)
-
+def load_data(data, category):
+    # data = read_csv(category)
+    # print("hi")
     hook = PostgresHook(postgres_conn_id="postgres_db_etl")
     conn = hook.get_conn()
     cursor = conn.cursor()
@@ -14,8 +13,9 @@ def load_data(category):
     try:
         values = data[data.columns].values.tolist()
 
-        with open(f"/opt/airflow/sql/insert/insert_{category}.sql", "r") as file:
+        with open(f"./sql/insert/insert_{category}.sql", "r") as file:
             query = file.read()
+            print(query)
         
         execute_values(cursor, query, values)
         conn.commit()
